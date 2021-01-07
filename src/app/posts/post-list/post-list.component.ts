b545ed96120000b6ce32033ee1c031a041dcd039
Raj-Kumar-Arora <Er_RajArora@yahoo.com>
2021-01-08 01:11:40 +0530

    11. Diving Into Structural Directives + fixed *ngFor not working by replacing : with = symbol (so silly mistake!)

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -7,22 +7,11 @@ import { Component } from "@angular/core";
 })
 
 export class PostListComponent {
-    posts : [
-        {title : 'first post title', content : 'this is first post\'s content.'},
-        {title : 'second post title', content : 'this is second post\'s content.'},
-        {title : 'third post title', content : 'this is third post\'s content.'}
-    ];
 
-    heroes: [
-        { id: 11, name: 'Dr Nice' },
-        { id: 12, name: 'Narco' },
-        { id: 13, name: 'Bombasto' },
-        { id: 14, name: 'Celeritas' },
-        { id: 15, name: 'Magneta' },
-        { id: 16, name: 'RubberMan' },
-        { id: 17, name: 'Dynama' },
-        { id: 18, name: 'Dr IQ' },
-        { id: 19, name: 'Magma' },
-        { id: 20, name: 'Tornado' }
-      ];
+    posts = [
+        // {title : 'first post title', content : 'this is first post\'s content.'},
+        // {title : 'second post title', content : 'this is second post\'s content.'},
+        // {title : 'third post title', content : 'this is third post\'s content.'}
+    ];
 }
+
