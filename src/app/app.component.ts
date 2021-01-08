b0f5e8d399b7c1ac65c68392ae993c55321d7768
Raj-Kumar-Arora <Er_RajArora@yahoo.com>
2021-01-09 03:09:53 +0530

    2.13 : Creating a Post Model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './posts/post.model';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mean-course';
 
-  storedPosts = [];
+  storedPosts: Post[] = [];
 
-  onPostAdded(post) {
+  onPostAdded(post: Post) {
     //alert ("Post added : " + post.title + " " + post.content);
 
     this.storedPosts.push(post);
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { Post } from "../post.model";
 
 @Component ({
     selector : 'app-post-list',
@@ -8,10 +9,6 @@ import { Component, Input } from "@angular/core";
 
 export class PostListComponent {
 
-    // constructor (){
-    //   alert ("post list : " + this.posts.length.toString());
-    // }
-
-    @Input() posts = [];
+    @Input() posts:Post[] = [];
 }
 
